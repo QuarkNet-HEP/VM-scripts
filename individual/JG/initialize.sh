dce67116f26d5073b3d7394787900b87d1992988
JG-QuarkNet <16691642+JG-QuarkNet@users.noreply.github.com>
2025-05-23 16:00:43 -0400

    Added 'ls' alias

--- individual/JG/initialize.sh
+++ individual/JG/initialize.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+user=$SUDO_USER
+
+# Create the `~/.bash_aliases` file
+touch /home/$user/.bash_aliases
+
+# Add the `ls` alias
+cat << EOF >> /home/$user/.bash_aliases
+alias ls='ls -Alh'
+EOF
+
