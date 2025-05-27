50fbd841a397d0975fd2a31158c58cc72735c00d
JG-QuarkNet <16691642+JG-QuarkNet@users.noreply.github.com>
2025-05-27 10:22:26 -0400

    Added .profile lines for sudoedit, journalctl

--- individual/JG/initialize.sh
+++ individual/JG/initialize.sh
@@ -16,5 +16,21 @@ cat << EOF >> /home/$user/.bash_aliases
 alias ls='ls -Alh'
 EOF
 
-# Activate the alias
-source /home/$user/.bash_aliases
+# Activate the alias only if it's needed for the current script.
+# This has no effect on the calling shell, and aliases aren't expanded in scripts.
+#source /home/$user/.bash_aliases
+
+# Make Emacs the editor for `sudoedit` and fix journalctl's junk
+cat << EOF >> /home/$user/.profile
+
+# Added $(date) - JG
+# Set the sudo editor to Emacs
+# https://old.reddit.com/r/linux/comments/osah05/ysk_do_not_use_sudo_vimnanoema\
+cs_to_edit_a_file/
+export EDITOR=/usr/bin/emacs
+
+# This makes journalctl wrap long lines
+# See https://serverfault.com/a/1067315 for explanation
+export SYSTEMD_LESS="FRXMK"
+
+EOF
