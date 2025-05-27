71da10f78334becd9e4ccc6148449a39dd202aa6
JG-QuarkNet <16691642+JG-QuarkNet@users.noreply.github.com>
2025-05-27 17:26:41 -0400

    Added Emacs installation

--- individual/JG/initialize.sh
+++ individual/JG/initialize.sh
@@ -4,9 +4,7 @@
 # $ sudo chmod 744 initialize.sh
 # $ ./initialize.sh && source ~/.bash_aliases
 
-# For the time being, this doesn't need 'sudo'.
-#user=$SUDO_USER
-user=$USER
+user=$SUDO_USER
 
 # Create the `~/.bash_aliases` file
 touch /home/$user/.bash_aliases
@@ -16,17 +14,15 @@ cat << EOF >> /home/$user/.bash_aliases
 alias ls='ls -Alh'
 EOF
 
-# Activate the alias only if it's needed for the current script.
-# This has no effect on the calling shell, and aliases aren't expanded in scripts.
-#source /home/$user/.bash_aliases
+# Install emacs
+/usr/local/sbin/apt-log install -y emacs-nox
 
 # Make Emacs the editor for `sudoedit` and fix journalctl's junk
 cat << EOF >> /home/$user/.profile
 
 # Added $(date) - JG
 # Set the sudo editor to Emacs
-# https://old.reddit.com/r/linux/comments/osah05/ysk_do_not_use_sudo_vimnanoema\
-cs_to_edit_a_file/
+# https://old.reddit.com/r/linux/comments/osah05/ysk_do_not_use_sudo_vimnanoemacs_to_edit_a_file/
 export EDITOR=/usr/bin/emacs
 
 # This makes journalctl wrap long lines
