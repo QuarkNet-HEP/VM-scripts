da981936b0613140e695e4f90c7157c2097b0c91
JG-QuarkNet <16691642+JG-QuarkNet@users.noreply.github.com>
2025-05-27 15:42:52 -0400

    Create apache-init.sh

---
+++ apache-init.sh
@@ -0,0 +1,14 @@
+#!/bin/bash                                                                   
+
+# Get the mod_php name, which varies by version.                              
+apache_mods='/etc/apache2/mods-available/'
+php_name=$(find $apache_mods -type f -name "php*.load" | xargs -n 1 basename)
+php_mod_name=${php_name%'.load'}
+
+# Enable mods the e-Labs require                                              
+sudo a2enmod $php_mod_name
+sudo a2enmod proxy
+sudo a2enmod proxy_ajp
+sudo a2enmod ssl
+sudo a2enmod rewrite
+sudo a2enmod headers
