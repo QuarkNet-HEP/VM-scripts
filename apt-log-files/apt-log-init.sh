8290eb01679fadb1d5edf4dd19ed0879f0863802
JG-QuarkNet <16691642+JG-QuarkNet@users.noreply.github.com>
2025-09-27 19:13:57 -0400

    Added check for download utility
    Added check for 'wget' (preferred) and 'curl', since either or both may be missing from a new system

--- apt-log-files/apt-log-init.sh
+++ apt-log-files/apt-log-init.sh
@@ -4,8 +4,16 @@
 # One-liner to retrieve and implement this script:
 # sudo curl -o apt-log-init.sh https://raw.githubusercontent.com/QuarkNet-HEP/VM-scripts/refs/heads/main/apt-log-files/apt-log-init.sh && sudo chmod 744 apt-log-init.sh && sudo bash -c 'source ./apt-log-init.sh' && sudo rm ./apt-log-init.sh && source /etc/bash.bashrc && source ~/.bashrc
 
-# Download and place the apt-log scripts
-wget -O /usr/local/sbin/apt-log https://raw.githubusercontent.com/QuarkNet-HEP/VM-scripts/refs/heads/main/apt-log-files/apt-log
+# Download and place the apt-log scripts.
+# Use `wget` if it's available, else `curl`.
+if command -v wget >/dev/null 2>&1; then
+    wget -O /usr/local/sbin/apt-log https://raw.githubusercontent.com/QuarkNet-HEP/VM-scripts/refs/heads/main/apt-log-files/apt-log
+elif command -v curl >/dev/null 2>&1; then
+    curl -o /usr/local/sbin/apt-log https://raw.githubusercontent.com/QuarkNet-HEP/VM-scripts/refs/heads/main/apt-log-files/apt-log
+else
+    echo "Neither 'wget' nor 'curl' were found on the system.  Exiting."
+    exit 1
+fi
 
 chown root:root /usr/local/sbin/apt-log
 chmod 744 /usr/local/sbin/apt-log
